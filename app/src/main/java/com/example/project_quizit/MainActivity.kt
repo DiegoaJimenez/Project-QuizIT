4b3d47db858f506c330ecd61623561111f2956da
goslip05 <djimenez750@uan.edu.co>
2023-05-07 00:33:17 -0500

    Se agredan binding y intents para cambio de actividades

--- app/src/main/java/com/example/project_quizit/MainActivity.kt
+++ app/src/main/java/com/example/project_quizit/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.project_quizit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project_quizit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.linkRegister.setOnClickListener { openRegister() }
+
+        binding.btnIniciarSesion.setOnClickListener { openHome() }
+    }
+
+
+    fun openRegister(){
+        val registerIntent = Intent(this,registroUsuairoActivity::class.java)
+        //teamInfoIntent.putExtra("TEAM_DATA",team)
+        startActivity(registerIntent)
+    }
+
+    fun openHome(){
+        val homeIntent = Intent(this,homeActivity::class.java)
+        //teamInfoIntent.putExtra("TEAM_DATA",team)
+        startActivity(homeIntent)
     }
 }
+
---
+++ app/src/main/java/com/example/project_quizit/registroUsuairoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.project_quizit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class registroUsuairoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_usuairo)
+    }
+}
