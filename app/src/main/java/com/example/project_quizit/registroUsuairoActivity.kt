30386ae6aacc86c016eab58e69dee16be3e74b7c
goslip05 <djimenez750@uan.edu.co>
2023-05-07 01:05:59 -0500

    Agregando menu

--- app/src/main/java/com/example/project_quizit/registroUsuairoActivity.kt
+++ app/src/main/java/com/example/project_quizit/registroUsuairoActivity.kt
@@ -2,11 +2,18 @@ package com.example.project_quizit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class registroUsuairoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro_usuairo)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_home, menu)
+        return true
+    }
+
 }
