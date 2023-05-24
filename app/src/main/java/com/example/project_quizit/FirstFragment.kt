646b781cd4fe10d0ef89b06d4958fcff6bd4f8cf
goslip05 <71677761+goslip05@users.noreply.github.com>
2023-05-24 00:36:13 -0500

    Finishing styles cards and links

--- app/src/main/java/com/example/project_quizit/FirstFragment.kt
+++ app/src/main/java/com/example/project_quizit/FirstFragment.kt
@@ -32,7 +32,7 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.textCount.setOnClickListener {
+        binding.textTomarQuiz1.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
